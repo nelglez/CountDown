6486cc8171d84053421cd89f49bb4543e2101071
Nelson Gonzalez <nelglezfl@gmail.com>
2019-05-01 12:48:49 -0400

    Add outlet connections. Set count down seconds.

--- Countdown/CountdownViewController.swift
+++ Countdown/CountdownViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class CountdownViewController: UIViewController {
-
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+        
+        print("Countdown: \(datePicker.countDownDuration)")
+    }
+    
 }
 
